aaffb12ed26ce04bb999438193d3d77099882799
lucio rendine <luciorendine69@gmail.com>
2020-12-15 09:34:34 +0100

    push

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 var km = parseInt(prompt('Quanti km vuoi devi fare?'));
 var age = parseInt(prompt('Quanti anni hai?'));
-var priceKm = parseInt(age * 0.21);
-// console.log(priceKmAge)
+var priceKm = km * 0.21;
+console.log(priceKm);
 var priceKmAge = 0;
 
-if (age < 17){
-  priceKmAge = priceKm - ((priceKm / 100) * 20)
+if (age < 18){
+  priceKmAge = priceKm - ((priceKm / 100) * 20);
 } else if (age > 65){
-  priceKmAge = priceKm - ((priceKm / 100) * 40)
-} if (17 < age < 65){
-  priceKmAge = priceKm
+  priceKmAge = priceKm - ((priceKm / 100) * 40);
+} else {
+  priceKmAge = priceKm;
 }
 
-console.log('Il prezzo é:' + priceKmAge)
+console.log('Il prezzo é:' + priceKmAge);
 
 var outputContainer = document.getElementById('output');
 outputContainer.innerText = 'Il prezzo é ' + priceKmAge ;
